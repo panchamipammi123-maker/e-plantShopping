4a987fc0b1ebbd74347af9e47b22ede032715620
panchamipammi123-maker <panchamipammi123@gmail.com>
2026-04-10 00:55:11 +0530

    Create AboutUs.jsx

---
+++ src/components/AboutUs.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function AboutUs() {
+  return (
+    <div>
+      <h2>About Paradise Nursery</h2>
+      <p>
+        Paradise Nursery is a plant store that sells indoor and outdoor houseplants.
+      </p>
+    </div>
+  );
+}
+
+export default AboutUs;
